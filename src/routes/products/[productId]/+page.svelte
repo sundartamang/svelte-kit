e8e7815c88ca3bdf4ea037610fa7e6a57aa138b4
sursasundar <sundar.tamang@sursatech.com>
2024-08-24 22:23:30 +0545

    feat: before and after navigation

--- src/routes/products/[productId]/+page.svelte
+++ src/routes/products/[productId]/+page.svelte
@@ -1,12 +1,25 @@
 <script>
     import { page } from "$app/stores";
-    import { goto } from "$app/navigation";
+    import {
+        goto,
+        replaceState,
+        beforeNavigate,
+        afterNavigate,
+    } from "$app/navigation";
 
     const { productId } = $page.params;
 
     const goToHomePage = () => {
-        goto("/");
+        goto("/", { replaceState: true });
     };
+
+    beforeNavigate((navigation) => {
+        console.log("before navigation ", navigation);
+    });
+
+    afterNavigate((navigation) => {
+        console.log("after navigation ", navigation);
+    });
 </script>
 
 <h1>Products detail of {productId}</h1>
