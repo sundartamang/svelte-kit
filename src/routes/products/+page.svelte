9e908bf7053449e3e516a99d396529e2445e4b3e
sursasundar <sundar.tamang@sursatech.com>
2024-08-24 15:02:34 +0545

    feat : normal routes, nested routes, dynamic routes

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Product list</h1>
+
+<ul>
+    <li>
+        <a href="">Product 1</a>
+    </li>
+    <li>
+        <a href="">Produt 2</a>
+    </li>
+    <li>
+        <a href="">Product 3</a>
+    </li>
+</ul>
---
+++ src/routes/products/[productId]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { page } from "$app/stores";
+
+    const { productId } = $page.params;
+</script>
+
+<h1>Product Details of {productId}</h1>
